1242b8062f48d3d6f483459a189af56d9afb3691
ShihaoDong1 <shihao.dong1@gmail.com>
2020-06-03 09:25:19 -0400

    Updates

---
+++ precache-manifest.18d7252ef77f370bf415886fc7f2e6b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d374e97cb0d250f5019d69a5664a4dc1",
+    "url": "/Assignment-6-Contact-Card/index.html"
+  },
+  {
+    "revision": "b9cb417251f94b305480",
+    "url": "/Assignment-6-Contact-Card/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "7bb40fa3b82b5a168687",
+    "url": "/Assignment-6-Contact-Card/static/js/2.2c7a8cfe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Assignment-6-Contact-Card/static/js/2.2c7a8cfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9cb417251f94b305480",
+    "url": "/Assignment-6-Contact-Card/static/js/main.501da862.chunk.js"
+  },
+  {
+    "revision": "afad99646941ac40c32b",
+    "url": "/Assignment-6-Contact-Card/static/js/runtime-main.4c832f1d.js"
+  }
+]);
